f57403b46405d8d1384af3763b1b480927b9de0d
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-21 19:35:54 +0200

    u

---
+++ src/components/Widget/Widget.jsx
@@ -0,0 +1,15 @@
+export const Widget = ({ click }) => {
+    return (
+        <div>
+        <h2>Please leave feedback</h2>
+        state =
+        {
+          <p>good: { click }</p> 
+          <p>neutral:  { click }</p>
+          <p>bad:  { click }</p>
+        }
+        <h2>Statistics</h2>
+      </div>
+    </div >
+    );
+};
