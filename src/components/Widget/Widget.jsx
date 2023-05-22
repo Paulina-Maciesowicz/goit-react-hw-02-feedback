80b9558621be8feb40f4e6a3009971cbb1d8c49c
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-05-22 14:37:35 +0200

    u

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,3 +1,15 @@
+// import React, { Component } from "react";
+
+// class MyClassComponent extends Component {
+//   static defaultProps = {};
+
+//   static propTypes = {};
+
+//   render() {
+//     return <div>Class Component</div>;
+//   }
+// }
+
 export const Widget = ({ click }) => {
     return (
         <div>
