246c20616f489fc79752ce6b714134c3c3881f3a
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-06-15 14:20:44 +0200

    u
    u

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -49,10 +49,6 @@ export class Counter extends React.Component {
     ).toFixed(0);
   }
 
-  // decrement() {
-  //   this.setState ({ counter: this.state.counter - 1})
-  // }
-
   render() {
     return (
       <>
@@ -84,27 +80,3 @@ export class Counter extends React.Component {
     );
   }
 }
-
-// import { useState } from 'react';
-
-// export function Counter() {
-//   const [count, setCount] = useState(0);
-//   const [neutral, setNeutral] = useState(0);
-//   const [bad, setBad] = useState(0);
-//   const total = count + neutral + bad;
-//   const percentage = count / total ;
-
-//   return (
-//     <div>
-//       <h2>Please leave feedback</h2>
-//       <button onClick={() => setCount(count + 1)}>Good</button>
-//       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-//       <button onClick={() => setBad(bad + 1)}>Bad</button>
-//       <p>Good: {count}</p>
-//       <p>Neutral: {neutral}</p>
-//       <p>Bad: {bad}</p>
-//       <p>Total: {total}</p>
-//       <p>Positive feedback: {percentage}</p>
-//     </div>
-//   );
-// }
